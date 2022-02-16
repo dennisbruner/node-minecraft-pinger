217ac2b4e8681dc8fd6cc6d5349a8592021bdc5b
Grayson-code <northernstarmusic10@gmail.com>
2022-02-16 20:45:02 +0530

    type definitions

---
+++ lib/index.d.ts
@@ -0,0 +1,42 @@
+declare module 'minecraft-pinger'
+
+export interface pingPromiseInterface {
+    description: {
+		text: string;
+		extra?: {
+			color?: string;
+			text: string;
+			bold?: boolean;
+			strikethrough?: boolean;
+			extra?: {
+				color: string;
+				text: string;
+			}[];
+		}[];
+	};
+	players: {
+		online: number;
+		max: number;
+		sample?: {name: string; id: string}[];
+	};
+	version: {name: string; protocol: number};
+	ping: number;
+	modinfo?: {type: string; modList: string[]};
+	favicon?: string;
+}
+
+
+/**
+ * Returns basic info about the server
+ * 
+ * @param {string} hostname The host name of the server
+ * @param {number} port Port of the server most servers default to 25565
+ */
+export declare  function pingPromise(hostname:string,port:number): Promise<pingPromiseInterface>;
+/**
+ * 
+ * @param { string } hostname The host name of the server  
+ * @param { number } port Port of the server most servers default to 25565
+ * @returns { pingPromiseInterface } The information of the server 
+ */
+export declare function ping(hostname:string, port:number ,params:(error:Error, result:pingPromiseInterface) => void)
